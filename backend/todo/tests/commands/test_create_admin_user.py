e536fc9bbf2a851b1cf7198606e5413a78b28271
Curtis Haynes <CCHaynes1122@gmail.com>
2021-08-28 21:49:29 -0700

    lint

--- backend/todo/tests/commands/test_create_admin_user.py
+++ backend/todo/tests/commands/test_create_admin_user.py
@@ -9,11 +9,11 @@ class CommandsTestCase(TestCase):
     @mock.patch.dict(os.environ, {"ADMIN_USERNAME": "admin"})
     def test_create_admin_user_exists(self):
         User.objects.create(username="admin", is_superuser=True)
-        call_command('create_admin_user')
+        call_command("create_admin_user")
         self.assertEquals(User.objects.count(), 1)
-    
+
     @mock.patch.dict(os.environ, {"ADMIN_USERNAME": "admin"})
     def test_create_admin_user_create(self):
         self.assertEquals(User.objects.count(), 0)
-        call_command('create_admin_user')
+        call_command("create_admin_user")
         self.assertEquals(User.objects.count(), 1)
