3cef9c0ced1f9cbaaef7cfd95acc02b1e971b8ae
Curtis Haynes <CCHaynes1122@gmail.com>
2021-08-28 21:28:59 -0700

    Add couple of tests and coverage package

---
+++ backend/todo/tests/commands/test_create_admin_user.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.core.management import call_command
+from django.test import TestCase
+from unittest import mock
+import os
+
+
+class CommandsTestCase(TestCase):
+    @mock.patch.dict(os.environ, {"ADMIN_USERNAME": "admin"})
+    def test_create_admin_user_exists(self):
+        User.objects.create(username="admin", is_superuser=True)
+        call_command('create_admin_user')
+        self.assertEquals(User.objects.count(), 1)
+    
+    @mock.patch.dict(os.environ, {"ADMIN_USERNAME": "admin"})
+    def test_create_admin_user_create(self):
+        self.assertEquals(User.objects.count(), 0)
+        call_command('create_admin_user')
+        self.assertEquals(User.objects.count(), 1)
